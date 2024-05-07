29a3588b5a726fbd4a324d8d485dfe16573cb62f
Riya Fartyal <riyaf3105@gmail.com>
2024-05-07 16:09:40 +0900

    Challenge completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # verb "url", to: "controller#action"
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  get "tasks", to: "tasks#index", as: :tasks
+
+  get "tasks/new", to: "tasks#new", as: :new_task
+
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  patch '/tasks/:id', to: 'tasks#update'
+
+  delete '/tasks/:id', to: 'tasks#destroy'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
